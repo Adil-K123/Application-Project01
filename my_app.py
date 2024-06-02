a02cdd85255a9ca7a80235d6d25fb6292d3d0f67
adilk <adilk@ubuntu-main.myguest.virtualbox.org>
2024-06-02 17:15:28 +0530

    updated web content

--- my_app.py
+++ my_app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def web_content():
-    return "**** Hello,This is a Git Automated Test Page ****"
+    return "**** Hello,This is a Git Automated Test Page_:) ****"
 
 if __name__ == "__main__":
     app.run(debug=True)
